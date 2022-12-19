ccf1134b92d480b469e13b704b6c7715888b6998
Urodich <maks.zhuravscky@mail.ru>
2022-12-19 18:29:34 +0500

    wqeqwe

---
+++ My project (1)/Assets/scripts/spheresParent_script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spheresParent_script : MonoBehaviour
+{
+    Quaternion staticRotation;
+
+    void Start()
+    {
+        staticRotation = gameObject.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.rotation = staticRotation;
+    }
+}
